b19324eb3843d660855243ff96462ad4ed80f478
igor <rogiviela@gmail.com>
2021-03-28 09:49:16 -0300

    28032021_094900

--- src/server.js
+++ src/server.js
@@ -4,52 +4,24 @@ require('./database');
 const Person = require('./models/Person');
 const Proposal = require('./models/Proposal');
 const Deputado = require('./models/Deputado');
-teste();
-async function teste() {
-    // if (1 == 1);
-    // var person = await Person.findOne({
-    //     where: { Id: '8275cfd0-8f40-11eb-89aa-63c8d81c7323' }
-    // });
-    // if (1 == 1);
-    // var proposal = await Proposal.findOne({
-    //     where: { Id: '8275cfd0-8f40-11eb-89aa-63c8d81c7322' }
-    // })
-
-    // var proposal = await person.createProposalsFK({ Type: 'PL', Number: '233', Year: '2021', Description: 'test igor insercao ' });
-    // if (1 == 1);
-    // person.addProl(proposal);
-    // if (1 == 1);
-    var person = await Person.findOne({
-        where: { Id: '8275cfd0-8f40-11eb-89aa-63c8d81c7323' }, include: 'ProposalsFK'
-    });
-    if (1 == 1);
-
-}
-// Person.findOne({
-//     where: { Id: '8275cfd0-8f40-11eb-89aa-63c8d81c7322' }
-// }).then
-//     .then((res) => {
-
-//        res.addProposal()
-
+// teste();
+// async function teste() {
+//     if (1 == 1);
+//     var person = await Person.findOne({
+//         where: { Id: '8275cfd0-8f40-11eb-89aa-63c8d81c7323' }
 //     });
+//     if (1 == 1);
+//     var proposal = await Proposal.findOne({
+//         where: { Id: '8275cfd0-8f40-11eb-89aa-63c8d81c7322' }
+//     })
 
-// Proposal.findOne({
-//     where: { Id: '8275cfd0-8f40-11eb-89aa-63c8d81c7322' }, include: 'PeopleFK'
-// })
-//     // Person.findAll({
-//     //     include: {
-//     //         model: Proposal,
-//     //         as: 'ProposalsFK'
-//     //     },
-//     //     where: {
-//     //         Name: 'Teste'
-//     //     }
-//     // })
-//     .then((res) => {
-//         if (1 == 1);
+//     var proposal = await person.addProposalsFK(proposal);
+//     if (1 == 1);
+//     // person.addProl(proposal);
+//     // if (1 == 1);
+//     // var person = await Person.findOne({
+//     //     where: { Id: '8275cfd0-8f40-11eb-89aa-63c8d81c7323' }, include: 'ProposalsFK'
+//     // });
+//     if (1 == 1);
 
-//     });
-// Person.create({ Name: 'Teste', FullName: 'Teste1' });
-// Deputado.create({ PersonId: '8275cfd0-8f40-11eb-89aa-63c8d81c7323', Name: 'Teste', FullName: 'Teste1' });
-if (1 == 1);
+// }
